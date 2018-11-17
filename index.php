1f0bba8c279e37cd6cb334edfac7b30d3a30a042
Kimi07Fan <45129469+Kimi07Fan@users.noreply.github.com>
2018-11-18 00:20:13 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<title>Three Player Battleship</title>
+<style>
+    body {
+        margin:0;
+    }
+    .image {
+        position:fixed;
+        width:100%;
+        height:100%;
+        background:green;
+        background-image:url('Homepage.png');
+        background-size:cover;
+    }
+    .play_button{
+        position: fixed;
+        left: 530px;
+        top: 200px;
+    }
+    .htp_button{
+        position: fixed;
+        top: 400px;
+        left: 530px;
+    }
+</style>
+</head>
+<body bgcolor = "black">
+<div class = "image"></div>
+	<?php
+		$mysqli = new mysqli("localhost", "root", "", "Battleship");
+			if($mysqli->connect_error)
+			{
+				$mysqli = new mysqli("localhost", "root", "");
+				$mysqli->query("CREATE DATABASE Battleship");
+				header('Location: index.php');
+			}
+		$mysqli->query("USE Battleship");
+	?>
+<center>
+<font color = "red">
+</font>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<form method = "get" action = "setup.html" class = "play_button">
+<!--input type = "submit" name = "Play!" value = "Play!" height "20px" width = "500px" color = "black"-->
+<input type = "image" src = "Play_button.png" alt = "submit" height = "100" width = "100">
+</form>
+<form method = "get" action = "htp.html" class = "htp_button">
+<input type = "image" src = "How_to_play.png" alt = "submit" height = "100" width = "200">
+</form>
+<center>
+</body>
+</html>
