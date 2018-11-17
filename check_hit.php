a80cd9387c3491e7c9972e93ebe947b9c816d2a7
Kimi07Fan <45129469+Kimi07Fan@users.noreply.github.com>
2018-11-18 00:25:14 +0530

    Create check_hit.php

---
+++ check_hit.php
@@ -0,0 +1,16 @@
+<?php 
+	$cell = $_GET['cell'];
+	$mysqli = new mysqli("localhost", "root", "", "Battleship");
+	$mysqli->query("USE Battleship");
+	$result = $mysqli->query("SELECT Ship FROM Board WHERE Positions LIKE \"%$cell%\"");
+		if($result->num_rows > 0)
+			{
+				$row = $result->fetch_assoc();
+						echo $row["Ship"];
+					if($row["Ship"] == 0)
+						echo "Miss";
+					else
+						echo "Hit";
+					
+			}
+?>
