f246aa85e7d8e27ed22ce48071a4940227a33cd5
Kimi07Fan <mehulbose09@gmail.com>
2018-11-25 22:11:51 +0530

    Update play_area.php

--- play_area.php
+++ play_area.php
@@ -484,16 +484,17 @@ function show_turn(id) {
     if (id == 3) { // Computer's Turn
         ac_h.setAttribute("style", "top: 605px; left: 800px; position: absolute");
     }
+    else {
+        var k;
+    }
     document.body.appendChild(ac_h);
 }
-function check_hit_2(cell, id1, id2, id3, turn)
-{
+function check_hit_2(cell, id1, id2, id3, turn){
     // alert("hit " + cell);
     if(turn == id1)
         location.href = 'check_hit.php?cell=' + cell + '&id1=' + id1 + '&id2=' + id2 + '&id3=' + id3 + '&B=2';
 }
-function check_hit_3(cell, id1, id2, id3, turn)
-{
+function check_hit_3(cell, id1, id2, id3, turn){
     // alert("hit " + cell);
     if(turn == id1)
         location.href = 'check_hit.php?cell=' + cell + '&id1=' + id1 + '&id2=' + id2 + '&id3=' + id3 + '&B=3';
@@ -825,13 +826,11 @@ function create_comp_board(){
     }
     // alert(passing);
 }
-function send_board(id1, id2)
-{
+function send_board(id1, id2){
     // alert("GOME");
     location.href = 'send_comp_board.php?id1=' + id1 + '&id2=' + id2 + '&array=' + passing;
 }
-function comp_hit_cell(id, id1, id2, id3, turn)
-{
+function comp_hit_cell(id, id1, id2, id3, turn){
     var place = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
     if(id == 1)
     {
